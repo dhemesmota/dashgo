d21c561e9a1bf1b9c156a6245e4fcf60dbd0ea85
Dhemes Mota <dhemes.mota@gmail.com>
2021-05-16 16:28:17 -0300

    add react-hook-form and validation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,33 @@ import {
   Button, 
   Stack, 
  } from "@chakra-ui/react";
+ import { SubmitHandler, useForm } from 'react-hook-form'
+ import * as yup from 'yup'
+ import { yupResolver } from '@hookform/resolvers/yup'
 
 import { Input } from '../components/Form/Input'
 
+type SignInFormData = {
+  email: string;
+  password: string;
+}
+
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória'),
+})
+
 export default function SignIn() {
+  const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm({
+    resolver: yupResolver(signInFormSchema)
+  })
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    console.log(values);
+  }
+
   return (
     <Flex 
       w="100vw" 
@@ -22,10 +45,23 @@ export default function SignIn() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-          <Input name="email" label="E-mail" type="email" />
-          <Input name="password" label="Senha" type="password" />
+          <Input 
+            name="email" 
+            label="E-mail" 
+            type="email"
+            error={errors.email}
+            {...register('email')} 
+          />
+          <Input 
+            name="password" 
+            label="Senha" 
+            type="password" 
+            error={errors.password}
+            {...register('password')}
+          />
         </Stack>
 
         <Button 
@@ -33,6 +69,7 @@ export default function SignIn() {
           mt="6" 
           colorScheme="pink"
           size="lg"
+          isLoading={isSubmitting}
         >
           Entrar
         </Button>
